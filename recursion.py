681bb4536a57d836d1ee45cbf45bdedd8f24547a
Disha-19k <dishakurale730@gmail.com>
2024-12-10 17:20:44 +0530

    Add files via upload

---
+++ recursion.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def recur_sum(n):
+    if(n<=1):
+        return n
+    else:
+        return n+recur_sum(n-1)
+num=int(input("enter the number"))
+if(num<0):
+    print("positive numbwe")
+else:
+    print("the sum is",recur_sum(num))
+
+
+# In[ ]:
+
+
+
+
